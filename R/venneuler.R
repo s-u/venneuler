51783c6837f1dd32b59ca7b6baeeaa4effdcb27f
Simon Urbanek <simon.urbanek@R-project.org>
2009-08-18 17:05:36 +0000

    add R code, basic docs and enhance VennData
    git-svn-id: https://svn.rforge.net/venneuler/trunk@3 8a2d8636-53ba-49bb-a8cb-dc32029d4a64

---
+++ R/venneuler.R
@@ -0,0 +1,75 @@
+venneuler <- function(combinations, weights, ...) {
+  if (missing(combinations)) stop("combinations must be specified")
+  if (inherits(combinations, "table")) {
+    if (!missing(weights)) warning("combinations is a table yet weights are also specified - ignoring weights")
+    weights <- as.vector(combinations)
+    rnm <- rep(rownames(combinations), dim(t)[2])
+    cnm <- rep(colnames(combinations), each=dim(t)[1])
+    names(weights) <- paste(rnm, cnm, sep="~")
+    if (all(weights == 0)) stop("all weights are zero")
+    weights <- weights[weights != 0]
+    combinations <- names(weights)
+  } else if (missing(weights) && is.numeric(combinations) && is.null(dim(combinations))) {
+    if (is.null(names(combinations))) stop("combinations are a numeric vector but without names")
+    weights <- combinations
+    combinations <- names(combinations)
+  }
+  if (is.matrix(combinations) && is.character(combinations) && dim(combinations)[2] == 2)
+    vd <- .jnew("edu/uic/ncdm/venn/data/VennData", as.character(combinations[,1]), as.character(combinations[,2]))
+  else {
+    if (!is.character(combinations)) stop("combinations must be either a character vector, a table, a named numeric vector or a character matrix with two columns")
+    if (missing(weights)) weights <- rep(1, length(combinations))
+    vd <- .jnew("edu/uic/ncdm/venn/data/VennData", as.character(combinations), as.double(weights))
+  }
+  a <- .jnew("edu/uic/ncdm/venn/VennAnalytic")
+  g <- .jcall(a, "Ledu/uic/ncdm/venn/VennDiagram;", "compute", vd)
+  ct <- lapply(.jevalArray(.jfield(g, "[[D", "centers", convert=FALSE)), .jevalArray)
+  n <- length(ct)
+  ct <- matrix(unlist(ct), ncol=2, byrow=TRUE)
+  colnames(ct) <- c("x", "y")
+  diam <- .jevalArray(.jfield(g, "[D", "diameters", convert=FALSE))
+  areas <- .jevalArray(.jfield(g, "[D", "areas", convert=FALSE))
+  res <- .jevalArray(.jfield(g, "[D", "residuals", convert=FALSE))
+  col <- .jevalArray(.jfield(g, "[D", "colors", convert=FALSE))
+  lab <- .jevalArray(.jfield(g, "[Ljava/lang/String;", "circleLabels", convert=FALSE))
+  rownames(ct) <- lab
+  names(diam) <- lab
+  names(col) <- lab
+  names(res) <- .jevalArray(.jfield(g, "[Ljava/lang/String;", "residualLabels", convert=FALSE))
+  structure(list(centers=ct, diameters=diam, colors=col, labels=lab, residuals=res,
+            stress=.jfield(g, "D", "stress"), stress01=.jfield(g, "D", "stress01"),
+	    stress05=.jfield(g, "D", "stress05")), class="VennDiagram")
+}
+
+## Note: in col.fn we need more croma and less luminance than usual, because we'll be plotting with reduced alpha
+plot.VennDiagram <- function(vd, col, col.fn = function(col) hcl(col * 360, 130, 60), alpha=0.3, main=NULL, edges=200, border=NA, col.txt=1, ...) {
+  # calculate total extents
+  xtp <- vd$centers + vd$diameters
+  xtm <- vd$centers - vd$diameters
+  xr <- range(c(xtp[,1], xtm[,1]))
+  yr <- range(c(xtp[,2], xtm[,2]))
+  # create canvas
+  plot.new()
+  plot.window(xr, yr, "", asp = 1)
+  # adjust alpha for all colors if specified
+  n <- length(vd$diameters)
+  if (missing(col)) col <- col.fn(vd$colors)
+  if (length(col) < n) col <- rep(col, length.out=n)
+  if (!is.na(alpha)) {
+    col <- col2rgb(col) / 255
+    col <- rgb(col[1,], col[2,], col[3,], alpha)
+  }
+  # prepare circle coordinates
+  s <- seq.int(edges) / edges * 2 * pi
+  sx <- cos(s)
+  sy <- sin(s)
+  if (!is.null(border)) border <- rep(border, length.out=n)
+  # plot all circles
+  for (i in seq.int(n))
+    polygon(vd$centers[i, 1] +  vd$diameters[i] * sx, vd$centers[i, 2] + vd$diameters[i] * sy, col = col[i], border = border[i])
+  # if col.txt is not NA, plot the circle text
+  if (!all(is.na(col.txt))) text(vd$centers, vd$labels, col=col.txt)
+  # finish with title
+  title(main = main, ...)
+  invisible(NULL)
+}
